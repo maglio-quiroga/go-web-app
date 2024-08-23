16eaa2a66c3d9eb6d93969c0089564c35bb1b8fa
Maglio <magliocquirogaf@gmail.com>
2024-08-23 01:18:17 -0400

    prueba del server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HOLA MUNDO")
+}
+
 func main() {
-	//pass
+	enrutador := mux.NewRouter()
+	enrutador.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+	enrutador.HandleFunc("/", Index)
+	http.ListenAndServe(":3000", enrutador)
 }
