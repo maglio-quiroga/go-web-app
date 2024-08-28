44c17f5de3a5025d3fc1b6cddcc46525006b5c90
Maglio <magliocquirogaf@gmail.com>
2024-08-28 16:04:50 -0400

    lista la navbar con su funcion responsive

--- aplicacion/src/Componentes/navbar/navbar.js
+++ aplicacion/src/Componentes/navbar/navbar.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React , {useState} from "react";
 import "./navbar.css";
 
 export const Navbar = () => {
+    const [estado , cambiarEstado] = useState(false)
+    const accionMenu = () => {
+        cambiarEstado(!estado)
+    }
     return (
         <header className="header">
             <a href="#" className="logo">Logo</a>
-            <nav className="navbar">
+            <nav className={`navbar ${estado ? "activo" : ""}`}>
                 <a href="/">Inicio</a>
                 <a href="/">Egresados</a>
                 <a href="/">Eventos</a>
                 <a href="/">Trabajos</a>
             </nav>
+            <button className="c-hamburguesa" onClick={ accionMenu }>
+                <i className="bi bi-list i-hamburguesa"></i>
+            </button>
         </header>
     )
 }
