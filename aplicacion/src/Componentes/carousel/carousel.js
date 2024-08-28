b8fea71025adbb55b5712888725a16258ffa18f8
Maglio <magliocquirogaf@gmail.com>
2024-08-28 16:34:43 -0400

    carrusel listo, abarca todo el ancho, hay que usar imagenes con alta resolucion

---
+++ aplicacion/src/Componentes/carousel/carousel.js
@@ -0,0 +1,28 @@
+import ImageGallery from "react-image-gallery"
+import "react-image-gallery/styles/css/image-gallery.css"
+
+export const GaleriaImagenes = () => {
+    const imagenes = [
+        {
+            original : "https://picsum.photos/id/1018/1920/1000",
+            thumbnail : "https://picsum.photos/id/1018/250/150",
+        },
+        {
+            original : "https://picsum.photos/id/1015/1920/1000",
+            thumbnail : "https://picsum.photos/id/1015/250/150",
+        },
+        {
+            original : "https://picsum.photos/id/1016/1920/1000",
+            thumbnail : "https://picsum.photos/id/1016/250/150",
+        },
+    ]
+    return (
+        <>
+            <ImageGallery
+                items={imagenes}
+                showPlayButton={false}
+                showThumbnails={false}
+            />
+        </>
+    )
+}
--- aplicacion/src/index.js
+++ aplicacion/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom/client'
 import {Navbar} from './Componentes/navbar/navbar'
+import {GaleriaImagenes} from './Componentes/carousel/carousel'
 import "./index.css"
 
 const root = ReactDom.createRoot(document.getElementById("root"))
 root.render(
     <>
         <Navbar/>
+        <GaleriaImagenes/>
     </>
 )
