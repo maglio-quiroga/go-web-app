b125091b4f2f9eea40355db4c007a598263606f5
Maglio <magliocquirogaf@gmail.com>
2024-09-02 14:47:20 -0400

    recuperacion de datos de alumnos listo, falta el de eventos y manejar el formulario

--- aplicacion/src/index.js
+++ aplicacion/src/index.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactDom from 'react-dom/client'
-import {Navbar} from './Componentes/navbar/navbar'
-import {GaleriaImagenes} from './Componentes/carousel/carousel'
-import {Separador} from './Componentes/separador/separador'
-import {Tarjeta} from './Componentes/tarjetas/alumnos'
-import {Calendario} from './Componentes/calendario/calendario'
-import {Formulario} from './Componentes/formulario/form'
-import {Footer} from './Componentes/footer/footer'
+import { Navbar } from './Componentes/navbar/navbar'
+import { GaleriaImagenes } from './Componentes/carousel/carousel'
+import { Separador } from './Componentes/separador/separador'
+import { Tarjetas } from './Componentes/tarjetas/alumnos'
+import { Calendario } from './Componentes/calendario/calendario'
+import { Formulario } from './Componentes/formulario/form'
+import { Footer } from './Componentes/footer/footer'
 import "./index.css"
 
-const info = async () => {
-    const response = await fetch("http://localhost:8080/inicio")
-    const data = await response.json()
-    return data
+const App = () => {
+    const [alumnos, setAlumnos] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://localhost:8080/inicio")
+            const data = await response.json()
+            setAlumnos(data.Alumnos)
+        }
+
+        fetchData()
+    }, [])
+
+    return (
+        <>
+            <Navbar/>
+            <GaleriaImagenes/>
+            <Separador nombre="Egresados Mas Recientes" />
+            <div className='card-container'>
+                <Tarjetas alumnos={alumnos} />
+            </div>
+            <Separador nombre="Proximos Eventos" />
+            <Calendario/>
+            <Separador nombre="Formulario Ex-Alumnos" />
+            <Formulario/>
+            <div className='fot'>
+                <Footer/>
+            </div>
+        </>
+    )
 }
 
-info().then(data => console.log(data));
-
 const root = ReactDom.createRoot(document.getElementById("root"))
-root.render(
-    <>
-        <Navbar/>
-        <GaleriaImagenes/>
-        <Separador
-            nombre = "Egresados Mas Recientes"
-        />
-        <div className='card-container'>
-        <Tarjeta></Tarjeta>
-        <Tarjeta></Tarjeta>
-        <Tarjeta></Tarjeta>
-        </div>
-        <Separador
-            nombre = "Proximos Eventos"
-        />
-        <Calendario></Calendario>
-        <Separador
-            nombre = "Formulario Ex-Alumnos"
-        />
-        <Formulario></Formulario>
-        <div className='fot'>
-            <Footer></Footer>
-        </div>
-    </>
-)
+root.render(<App />)
